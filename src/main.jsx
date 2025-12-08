6d36157f4b4f3f06f48d893b788d98285288871e
Sultan Amiour <sultan.amiour@epita.fr>
2025-12-08 18:42:08 +0100

    chore: add packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// React lib manages Virtual DOM. It handles components, states, and performs
+// the "Diffing" algorithm to compare this DOM with the actual DOM.
+import React from 'react'
+// ReactDOM manages the Actual DOM. It receives the instructions from React to render into the
+// Actual DOM.
+import ReactDOM from 'react-dom/client'
+// Global styling (for all components)
 import './index.css'
+// Whole application layout (with its components)
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+    // Strict Mode is a wrapper of your app that checks for unsafe or deprecated usages
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+      tailwindcss(),
+      react(),
+    ],
+    server: {
+        port: 3000,
+        // what this does is that whenever the client hits /api/jobs, it's really going to hit
+        // localhost:8000/procedures
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            }
+        }
+    }
 })
