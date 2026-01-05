d76a928e7be2ac6fb2284792455d56da9b675cf1
Joseph Ayoub <50584471+snapydoodoo@users.noreply.github.com>
2026-01-05 03:16:19 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = ({ scrollToDoc, lang = 'en' }) => {
                 <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
                     
                     {/* Face Header */}
-                    <div className="text-center mt-6 mb-12">
+                    <div className="text-center mt-6 mb-12 pt-12">
                       <svg className="w-12 h-12 mx-auto mb-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                         <path fill="#312e81" d="M128 128C128 92.7 156.7 64 192 64L341.5 64C358.5 64 374.8 70.7 386.8 82.7L493.3 189.3C505.3 201.3 512 217.6 512 234.6L512 512C512 547.3 483.3 576 448 576L192 576C156.7 576 128 547.3 128 512L128 128zM336 122.5L336 216C336 229.3 346.7 240 360 240L453.5 240L336 122.5zM216 128C202.7 128 192 138.7 192 152C192 165.3 202.7 176 216 176L264 176C277.3 176 288 165.3 288 152C288 138.7 277.3 128 264 128L216 128zM216 224C202.7 224 192 234.7 192 248C192 261.3 202.7 272 216 272L264 272C277.3 272 288 261.3 288 248C288 234.7 277.3 224 264 224L216 224zM286.3 384C275 384 264.4 389.1 257.4 397.9L197.3 473C189 483.3 190.7 498.5 201 506.7C211.3 514.9 226.5 513.3 234.7 502.9L281.8 444.1L297 494.8C300 505 309.4 511.9 320 511.9L424 511.9C437.3 511.9 448 501.2 448 487.9C448 474.6 437.3 463.9 424 463.9L337.9 463.9L321.8 410.3C317.1 394.6 302.7 383.9 286.3 383.9z"/>
                       </svg>
@@ -93,10 +93,10 @@ const Hero = ({ scrollToDoc, lang = 'en' }) => {
                               {translate(uitext.heroImportantNoticeDesc, lang)}
                             </p>
 
-                            <section className="m-auto max-w-lg px-1">
+                            <section className="m-auto px-1 flex justify-center">
                               <button
                                 onClick={() => navigate(`/${lang}/contact`)}
-                                className="flex justify-center items-center gap-2 bg-[#441306] dark:bg-[#6A2C1F] text-orange-50 shadow-md font-semibold text-center py-3 px-6 rounded-xl hover:bg-[#542214] dark:hover:bg-[#7F3B2B] cursor-pointer"
+                                className="flex justify-center items-center w-full gap-2 bg-[#441306] dark:bg-[#6A2C1F] text-orange-50 shadow-md font-semibold text-center py-3 px-6 rounded-xl hover:bg-[#542214] dark:hover:bg-[#7F3B2B] cursor-pointer"
                               >
                                 <Phone className="w-5 h-5 text-red-600 dark:text-red-400" />
                                 {translate(uitext.contactUs, lang)}
